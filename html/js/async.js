d162febc7a7349d41a51ac0476f004ebc71669f6
Suchan Lee <lsc1814@naver.com>
2023-06-16 17:02:32 +0900

    commit learn async in Promise

--- html/js/async.js
+++ html/js/async.js
@@ -1,11 +1,21 @@
 f = async () => {
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("완료!"), 1000);
-  });
-  // 프라미스가 이행될 때까지 기다림 (*) async 함수에서는 await를 쓸 수 있되, promise 앞에만 쓸 수 있다.
-  let result = await promise;
-
-  alert(result); // "완료!"
+  Promise.allSettled([
+    new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(1);
+      }, 3000)
+    ),
+    new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(2);
+      }, 2000)
+    ),
+    new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(3);
+      }, 1000)
+    ),
+  ]);
 };
 
-f();
+console.log(f());
